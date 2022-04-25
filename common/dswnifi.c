0da8035b919d821e00d431118d12d3253925de3e
Coto <programmer.coto.1988@gmail.com>
2022-04-24 22:48:58 -0400

    merged TGDS-multiplayer-example into TGDS-unittest

--- common/dswnifi.c
+++ common/dswnifi.c
@@ -75,6 +75,8 @@ bool TGDSRecvHandlerUser(struct frameBlock * frameBlockRecv, int DSWnifiMode){
 		//NIFI local
 		case(dswifi_localnifimode):{
 			clrscr();
+			printf(" ---- ");
+			printf(" ---- ");
 			printf("DSWNIFIStatus:LocalNifi!");
 			return true;
 		}
@@ -83,6 +85,8 @@ bool TGDSRecvHandlerUser(struct frameBlock * frameBlockRecv, int DSWnifiMode){
 		//UDP NIFI
 		case(dswifi_udpnifimode):{
 			clrscr();
+			printf(" ---- ");
+			printf(" ---- ");
 			printf("DSWNIFIStatus:UDPNifi!");
 			return true;
 		}
@@ -108,6 +112,22 @@ void OnDSWIFIudpnifiEnable(){
 
 void OnDSWIFIGDBStubEnable(){
 
+}
+
+//UDP Nifi:
+//Step 1: TGDS Project is asked for Remote Companion's IP (AKA: WAN Remote TCP/IP)
+void ONDSWIFI_UDPNifiInvalidIP(char * targetIP){
+	
+}
+
+//Step 2: TGDS Project connected successfully to Remote Companion
+void ONDSWIFI_UDPNifiRemoteServerConnected(char * targetIP){
+	
+}
+
+//Step 3: TGDS Project connected successfully to another DS implementing the DSWNFI protocol
+void ONDSWIFI_UDPNifiExternalDSConnected(char * externalDSIP){
+	
 }
 
 //GDBStub Callbacks
