7428f5777c5a5d5616d94588257a4d504c9155fb
cotodevel <programmer.coto.1988@gmail.com>
2024-02-29 22:12:05 -0300

    -void handleDSInitError(int stage, u32 fwNo) is now fused with general exceptions (blue screen) -Re-enable MPU exceptions, forgot to add them back like 2 years ago, lol. I was wondering why MPU wasn't throwing exceptions. -fix ARGV segfaults 1
    -ARM7DLDI no longer initializes from FIFO IRQ, spinlocks instead.
    -Should fix booting on Slot 1 devices on nearly any kernel
    -Note: More commits to address TGDS homebrew compatibility & issues to come
    Commit: 2/?
    
    -reduce ARM9 init spinlocks to just one, will improve bootrate across all DS systems regardless of detected firmware.
    
    Note:
    as a side effect, the firmware detection code is removed,
    because keeping a whitelist of firmwares, rendered TGDS homebrew
    useless on certain DS systems, which is unacceptable.
    Thus this commit restores TGDS homebrew running on every DS system out there.
    
    -handle FS initialization failure as a DS exception always (stage 1, ARM7DLDI fail)

--- arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
+++ arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
@@ -32,12 +32,10 @@ USA
 	////////[Custom Memory implementation ]////////
 
 //Definition that overrides the weaksymbol expected from toolchain to init ARM9's TGDS memory allocation
-struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7MallocStartAddress, int ARM7MallocSize, bool isCustomTGDSMalloc, u32 TargetARM7DLDIAddress){
+struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(bool isCustomTGDSMalloc){
 	struct AllocatorInstance * customMemoryAllocator = &CustomAllocatorInstance;
 	memset((u8*)customMemoryAllocator, 0, sizeof(CustomAllocatorInstance));
 	customMemoryAllocator->customMalloc = isCustomTGDSMalloc;
-	customMemoryAllocator->ARM7MallocStartAddress = ARM7MallocStartAddress;
-	customMemoryAllocator->ARM7MallocSize = ARM7MallocSize;
 	
 	customMemoryAllocator->ARM9MallocStartaddress = (u32)sbrk(0);
 	customMemoryAllocator->memoryToAllocate = (1500*1024);	//1.5MB Alloc
@@ -53,15 +51,10 @@ struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7Malloc
 	XmemSetup(xmemsize, XMEM_BS);
 	XmemInit(customMemoryAllocator->ARM9MallocStartaddress, (u32)customMemoryAllocator->memoryToAllocate);
 	
-	//DLDI
-	customMemoryAllocator->DLDI9StartAddress = (u32)&_io_dldi_stub;
-	customMemoryAllocator->TargetARM7DLDIAddress = TargetARM7DLDIAddress;
-	
 	//Memory Setup: 
 	//NTR: ARM7 TGDS 64K = 0x03800000 ~ 0x03810000. TGDS Sound Streaming code: Enabled
 	//TWL: ARM7 TGDS 96K = 0x037f8000 ~ 0x03810000. TGDS Sound Streaming code: Disabled/Custom
-	if(__dsimode == true){
-		WRAM_CR = WRAM_0KARM9_32KARM7;
-	}
+	WRAM_CR = WRAM_0KARM9_32KARM7;
+	
 	return customMemoryAllocator;
 }
